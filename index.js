6cb34648286096c4145eaee3a4f08b81a950a7df
saurabh kumar <saurabhkumargga@gmail.com>
2024-07-16 17:54:22 +0530

    add forntend

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 require("dotenv").config();
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
+const express = require("express");
+const route = require("./Router/route");
+const app = express();
 let genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 const model = genAI.getGenerativeModel({model: "gemini-1.5-flash"})
 
+app.use("/", route);
 
-async function run(){
-    const prompt = "hi";
+// async function run(){
+//     const prompt = "hi";
 
-    const result = await model.generateContent(prompt);
-    const response = await result.response.text();
+//     const result = await model.generateContent(prompt);
+//     const response = await result.response.text();
 
-    console.log(response);
-}
+//     console.log(response);
+// }
 
-run();
+// run();
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port 3000");
+});
 
